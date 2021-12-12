b5f84fd094d9dbbaedfe39db941ecd1cfd79174c
Mason <github-20201224@wdy.one>
2021-12-13 00:53:00 +0800

    refactor: change config file format and support multiple subscriptions

--- go.mod
+++ go.mod
@@ -3,22 +3,35 @@ module 591-notifier
 go 1.17
 
 require (
-	github.com/bwmarrin/discordgo v0.23.2 // indirect
-	github.com/chromedp/cdproto v0.0.0-20211205231339-d2673e93eee4 // indirect
-	github.com/chromedp/chromedp v0.7.6 // indirect
+	github.com/chromedp/cdproto v0.0.0-20211205231339-d2673e93eee4
+	github.com/chromedp/chromedp v0.7.6
+	github.com/spf13/viper v1.10.0
+	gorm.io/driver/sqlite v1.2.6
+	gorm.io/gorm v1.22.4
+)
+
+require (
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
-	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
-	golang.org/x/crypto v0.0.0-20181030102418-4d3f4d9ffa16 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
-	gorm.io/driver/sqlite v1.2.6 // indirect
-	gorm.io/gorm v1.22.4 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"encoding/json"
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 )
@@ -31,9 +30,22 @@ type House struct {
 	Price   string
 }
 
-func main() {
-	initEnv()
+var subscriptions []map[string]string
 
+func init() {
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = viper.UnmarshalKey("subscriptions", &subscriptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", false),
 	)
@@ -44,88 +56,79 @@ func main() {
 	ctx, cancel = chromedp.NewContext(ctx)
 	defer cancel()
 
-	err := chromedp.Run(ctx, chromedp.Navigate(os.Getenv("SEARCH_URL")))
-	if err != nil {
-		log.Fatal(err)
-	}
 	for {
-		log.Printf("Start retrieving new houses...")
-		var rentItems []*cdp.Node
-		err = chromedp.Run(ctx,
-			chromedp.WaitVisible("#rent-list-app"),
-			chromedp.Nodes("section.vue-list-rent-item", &rentItems),
-		)
-		if err != nil {
-			log.Fatal(err)
+		for _, subscription := range subscriptions {
+			subscriptionName := subscription["name"]
+			log.Printf("Start retrieving new houses for subscription '%s' ...", subscriptionName)
+
+			err := chromedp.Run(ctx, chromedp.Navigate(subscription["searchUrl"]))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var rentItems []*cdp.Node
+			err = chromedp.Run(ctx,
+				chromedp.WaitVisible("#rent-list-app"),
+				chromedp.Nodes("section.vue-list-rent-item", &rentItems),
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+			db := initDb()
+
+			var newLinks []string
+			for _, rentItem := range rentItems {
+				var house House
+				house.Id = rentItem.AttributeValue("data-bind")
+				err = chromedp.Run(ctx, chromedp.Text("div.item-title", &house.Title, chromedp.ByQuery, chromedp.FromNode(rentItem)))
+				if err != nil {
+					log.Fatal("Failed to retrieve item title")
+				}
+
+				var ok bool
+				err = chromedp.Run(ctx, chromedp.AttributeValue("a", "href", &house.Link, &ok, chromedp.ByQuery, chromedp.FromNode(rentItem)))
+				if err != nil || !ok {
+					log.Fatal("Failed to retrieve item link")
+				}
+
+				var itemStyles []*cdp.Node
+				err = chromedp.Run(ctx, chromedp.Nodes("ul.item-style > li", &itemStyles, chromedp.ByQueryAll, chromedp.FromNode(rentItem)))
+				if err != nil {
+					log.Fatal("Failed to retrieve item style")
+				}
+				house.Type = itemStyles[0].Children[0].NodeValue
+				house.Layout = itemStyles[1].Children[0].NodeValue
+				house.Size = itemStyles[2].Children[0].NodeValue
+				house.Floor = itemStyles[3].Children[0].NodeValue
+
+				err = chromedp.Run(ctx, chromedp.Text("div.item-area > a", &house.Area, chromedp.ByQuery, chromedp.FromNode(rentItem)))
+				if err != nil {
+					log.Fatal("Failed to retrieve item area")
+				}
+
+				err = chromedp.Run(ctx, chromedp.Text("div.item-area > span", &house.Address, chromedp.ByQuery, chromedp.FromNode(rentItem)))
+				if err != nil {
+					log.Fatal("Failed to retrieve item address")
+				}
+
+				err = chromedp.Run(ctx, chromedp.Text("div.item-price", &house.Price, chromedp.ByQuery, chromedp.FromNode(rentItem)))
+				if err != nil {
+					log.Fatal("Failed to retrieve item price")
+				}
+				if db.Create(&house).Error != nil {
+					continue
+				}
+				newLinks = append(newLinks, house.Link)
+			}
+
+			if len(newLinks) != 0 {
+				sendToDiscord(subscriptionName, subscription["discordWebhookUrl"], newLinks)
+			}
 		}
-		db := initDb()
 
-		var newLinks []string
-		for _, rentItem := range rentItems {
-			var house House
-			house.Id = rentItem.AttributeValue("data-bind")
-			err = chromedp.Run(ctx, chromedp.Text("div.item-title", &house.Title, chromedp.ByQuery, chromedp.FromNode(rentItem)))
-			if err != nil {
-				log.Fatal("Failed to retrieve item title")
-			}
-
-			var ok bool
-			err = chromedp.Run(ctx, chromedp.AttributeValue("a", "href", &house.Link, &ok, chromedp.ByQuery, chromedp.FromNode(rentItem)))
-			if err != nil || !ok {
-				log.Fatal("Failed to retrieve item link")
-			}
-
-			var itemStyles []*cdp.Node
-			err = chromedp.Run(ctx, chromedp.Nodes("ul.item-style > li", &itemStyles, chromedp.ByQueryAll, chromedp.FromNode(rentItem)))
-			if err != nil {
-				log.Fatal("Failed to retrieve item style")
-			}
-			house.Type = itemStyles[0].Children[0].NodeValue
-			house.Layout = itemStyles[1].Children[0].NodeValue
-			house.Size = itemStyles[2].Children[0].NodeValue
-			house.Floor = itemStyles[3].Children[0].NodeValue
-
-			err = chromedp.Run(ctx, chromedp.Text("div.item-area > a", &house.Area, chromedp.ByQuery, chromedp.FromNode(rentItem)))
-			if err != nil {
-				log.Fatal("Failed to retrieve item area")
-			}
-
-			err = chromedp.Run(ctx, chromedp.Text("div.item-area > span", &house.Address, chromedp.ByQuery, chromedp.FromNode(rentItem)))
-			if err != nil {
-				log.Fatal("Failed to retrieve item address")
-			}
-
-			err = chromedp.Run(ctx, chromedp.Text("div.item-price", &house.Price, chromedp.ByQuery, chromedp.FromNode(rentItem)))
-			if err != nil {
-				log.Fatal("Failed to retrieve item price")
-			}
-			if db.Create(&house).Error != nil {
-				continue
-			}
-			newLinks = append(newLinks, house.Link)
-		}
-
-		sendToDiscord(newLinks)
-
-		log.Printf("Round finished, sleeping for 5 minutes...")
-		time.Sleep(5 * time.Minute)
-		err = chromedp.Run(ctx, chromedp.Reload())
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func initEnv() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Printf(".env file not found, skipping...")
-	}
-	if os.Getenv("SEARCH_URL") == "" {
-		log.Fatal("SEARCH_URL env not set, exiting...")
-	}
-	if os.Getenv("DISCORD_WEBHOOK_URL") == "" {
-		log.Fatal("DISCORD_WEBHOOK_URL env not set, exiting...")
+		sleepDuration := viper.GetDuration("refreshIntervalMinutes")
+		log.Printf("Round finished, sleeping for %d minutes...", sleepDuration)
+		time.Sleep(sleepDuration * time.Minute)
 	}
 }
 
@@ -140,15 +143,15 @@ func initDb() *gorm.DB {
 	return db
 }
 
-func sendToDiscord(links []string) *http.Response {
+func sendToDiscord(name string, webhookUrl string, links []string) *http.Response {
 	log.Printf("Sending %d new houses to discord...", len(links))
-	body := map[string]string{"content": strings.Join(links, "\n\n")}
+	body := map[string]string{"content": "Subscription name: " + name + "\n\n" + strings.Join(links, "\n\n")}
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
 		log.Fatal("Failed to encode request body", err)
 	}
 	resp, err := http.Post(
-		os.Getenv("DISCORD_WEBHOOK_URL"),
+		webhookUrl,
 		"application/json",
 		bytes.NewReader(jsonBody),
 	)
